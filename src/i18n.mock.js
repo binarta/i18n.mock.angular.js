a69409f2064e49cc03f0e0d4c6623ae37ebb9947
sgo <sgo@thinkerit.be>
2014-06-04 12:22:27 +0200

    introducing localeResolver stub

--- src/i18n.mock.js
+++ src/i18n.mock.js
@@ -1,2 +1,3 @@
 angular.module('i18n', [])
-    .factory('i18nLocation', function($location) {return $location;});
+    .factory('i18nLocation', function($location) {return $location;})
+    .factory('localeResolver', function() {return function() {return 'en'}});
--- test/i18n.mock.spec.js
+++ test/i18n.mock.spec.js
@@ -5,4 +5,8 @@ describe('i18n.mock', function() {
     it('i18nLocation is alias for $location', inject(function(i18nLocation, $location) {
         expect(i18nLocation).toEqual($location);
     }));
+
+    it('localeResolver returns en by default', inject(function(localeResolver) {
+        expect(localeResolver()).toEqual('en');
+    }));
 });
